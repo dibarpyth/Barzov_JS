650bb18044a164c8b6b31673913dc094631b09c2
DmitryB <seo@new4p.com>
2022-06-04 14:58:03 +0300

    TZ3: Patterns to get an array from any kind of input added. Some refactoring.

--- main.js
+++ main.js
@@ -20,16 +20,16 @@ function tz2(inputData) {
 }
 
 function tz3(inputData) {
-	let numbersArray = [];
-	if (inputData.includes(' ')) {
-		numbersArray = inputData.split(' ');
-		console.log('from spaces: ', numbersArray);
-	}
-	// if (inputData.includes(',')) {
-	// 	numbersArray = inputData.split(',');
-	// 	console.log('from commas: ', numbersArray);
-	// }
-	const resultArray = numbersArray.filter(el => el % 3 === 0);
+	// inputData = '1,2, 3 , 4, 5 6 7 , , ,, 8   9,  10,,11  12,13,14,15';
+	// inputData = '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]';
+	// inputData = "['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']";
+
+	const clearData = inputData
+		.replace(/['"\(\)\[\]\{\}]/g, '')
+		.replace(/[,;:]/g, ' ');
+	const numbersArray = clearData.split(' ');
+
+	const resultArray = numbersArray.filter(el => +el && el % 3 === 0);
 
 	generateOutput('tz3', resultArray);
 }
